906466dba795acb6a6e9e9ee45ce2db78322b0ef
fhelwanger <fhelwanger@gmail.com>
2016-07-31 21:47:32 -0300

    Make them move :dancers:

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import Node from './Node';
 
@@ -21,20 +21,83 @@ const styles = StyleSheet.create({
   },
 });
 
-const Canvas = () => (
-  <div className={css(styles.scroll)}>
-    <div className={css(styles.container)}>
-      <svg
-        className={css(styles.canvas)}
-        height="400"
-        width="600"
-      >
-        <Node x="70" y="50" />
-        <Node x="270" y="80" />
-        <Node x="200" y="230" />
-      </svg>
-    </div>
-  </div>
-);
+const nodes = [
+  { id: 'RAIN', states: ['T', 'F'], position: { x: 70, y: 50 } },
+  { id: 'SPRINKLER', states: ['T', 'F'], position: { x: 270, y: 80 } },
+  { id: 'GRASS_WET', states: ['T', 'F'], position: { x: 200, y: 230 } },
+];
+
+class Canvas extends Component {
+  movingNode = null;
+
+  handleNodeMouseDown = (node, e) => {
+    this.movingNode = {
+      id: node.id,
+      initialPosition: {
+        x: node.position.x,
+        y: node.position.y,
+      },
+      initialMousePosition: {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    };
+  };
+
+  handleMouseMove = e => {
+    if (this.movingNode == null) {
+      return;
+    }
+
+    const { id, initialPosition, initialMousePosition } = this.movingNode;
+
+    const difX = e.clientX - initialMousePosition.x;
+    const difY = e.clientY - initialMousePosition.y;
+
+    const newX = initialPosition.x + difX;
+    const newY = initialPosition.y + difY;
+
+    nodes.find(x => x.id === id).position = {
+      x: newX,
+      y: newY,
+    };
+
+    this.forceUpdate();
+  };
+
+  handleMouseUpOrLeave = () => {
+    this.movingNode = null;
+  };
+
+  renderNode = node => (
+    <Node
+      key={node.id}
+      id={node.id}
+      onMouseDown={e => this.handleNodeMouseDown(node, e)}
+      states={node.states}
+      x={node.position.x}
+      y={node.position.y}
+    />
+  );
+
+  render() {
+    return (
+      <div className={css(styles.scroll)}>
+        <div className={css(styles.container)}>
+          <svg
+            className={css(styles.canvas)}
+            onMouseMove={this.handleMouseMove}
+            onMouseUp={this.handleMouseUpOrLeave}
+            onMouseLeave={this.handleMouseUpOrLeave}
+            height="400"
+            width="600"
+          >
+            {nodes.map(this.renderNode)}
+          </svg>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Canvas;
--- src/components/Node.js
+++ src/components/Node.js
@@ -7,34 +7,37 @@ const styles = StyleSheet.create({
   },
 });
 
+const renderState = (state, index) => (
+  <text key={state} x="5" y={37 + (18 * index)}>
+    <tspan>{state}</tspan>
+    <tspan x="155" textAnchor="end">99.9</tspan>
+  </text>
+);
+
 const Node = props => (
   <g
-    transform={`translate(${props.x} ${props.y})`}
     className={css(styles.node)}
+    onMouseDown={props.onMouseDown}
+    transform={`translate(${props.x} ${props.y})`}
   >
     <rect
-      height={20 + (20 * 2)}
+      height={20 + (20 * props.states.length)}
       width="160"
       fill="#ff8"
       stroke="#333"
     />
 
-    <text x="5" y="15">NODE</text>
+    <text x="5" y="15">{props.id}</text>
     <path d="M0,20 h160" stroke="#333" />
 
-    <text x="5" y="37">
-      <tspan>True</tspan>
-      <tspan x="155" textAnchor="end">95.1</tspan>
-    </text>
-
-    <text x="5" y="55">
-      <tspan>False</tspan>
-      <tspan x="155" textAnchor="end">4.9</tspan>
-    </text>
+    {props.states.map(renderState)}
   </g>
 );
 
 Node.propTypes = {
+  id: PropTypes.string.isRequired,
+  onMouseDown: PropTypes.func,
+  states: PropTypes.arrayOf(PropTypes.string).isRequired,
   x: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
   y: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
 };
