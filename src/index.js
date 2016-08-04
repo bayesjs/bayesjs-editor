0fa45a926c62c07dd948bb3655ea4fb66cd27471
fhelwanger <fhelwanger@gmail.com>
2016-08-03 21:12:19 -0300

    Move network to a separate file and add CPTs

--- src/index.js
+++ src/index.js
@@ -3,34 +3,12 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import configureStore from './store/configureStore';
+import network from './networks/rain-sprinkler-grasswet';
 
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.css';
 
-const preloadedState = {
-  nodes: [
-    {
-      id: 'RAIN',
-      states: ['T', 'F'],
-      parents: [],
-      position: { x: 395, y: 80 },
-    },
-    {
-      id: 'SPRINKLER',
-      states: ['T', 'F'],
-      parents: ['RAIN'],
-      position: { x: 160, y: 90 },
-    },
-    {
-      id: 'GRASS_WET',
-      states: ['T', 'F'],
-      parents: ['RAIN', 'SPRINKLER'],
-      position: { x: 230, y: 235 },
-    },
-  ],
-};
-
-const store = configureStore(preloadedState);
+const store = configureStore(network);
 
 render(
   <Provider store={store}>
