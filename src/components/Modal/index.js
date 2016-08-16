2c5d89799a7f015df818a7c212e0f16fc72b0c5b
fhelwanger <fhelwanger@gmail.com>
2016-08-15 22:48:00 -0300

    Add modal title

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 import ReactModal from 'react-modal';
+import classNames from 'classnames';
+import styles from './styles.css';
 
 const style = {
   overlay: {
@@ -11,12 +13,15 @@ const style = {
     right: 'auto',
     bottom: 'auto',
     transform: 'translate(-50%, -50%)',
-    border: 'solid 1px #ccc',
+    border: 'solid 1px #333',
     borderRadius: 0,
+    padding: 0,
+    margin: 0,
   },
 };
 
 const Modal = ({
+  title,
   isOpen,
   onRequestClose,
   children,
@@ -26,11 +31,22 @@ const Modal = ({
     onRequestClose={onRequestClose}
     style={style}
   >
-    {children}
+    <div className={styles.header}>
+      {title}
+      <i
+        onClick={onRequestClose}
+        className={classNames(['fa', 'fa-times', styles.closeButton])}
+      />
+    </div>
+
+    <div className={styles.body}>
+      {children}
+    </div>
   </ReactModal>
 );
 
 Modal.propTypes = {
+  title: PropTypes.string.isRequired,
   isOpen: PropTypes.bool.isRequired,
   onRequestClose: PropTypes.func.isRequired,
   children: PropTypes.any.isRequired,
