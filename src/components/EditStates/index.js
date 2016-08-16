88d6fd6f78d85e255782f330d1b4733de98693ec
fhelwanger <fhelwanger@gmail.com>
2016-08-16 00:31:42 -0300

    Edit states

--- src/components/EditStates/index.js
+++ src/components/EditStates/index.js
@@ -1,26 +1,104 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Modal from '../Modal';
+import Button from '../Button';
+import styles from './styles.css';
 
-const EditStates = ({
-  node,
-  onRequestClose,
-}) => {
-  let children = null;
+class EditStates extends Component {
+  state = {
+    states: [],
+  };
 
-  if (node != null) {
-    children = <h1>{node.id}!</h1>;
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.node != null && this.props.node == null) {
+      this.setState({ states: [...nextProps.node.states] });
+    }
   }
 
-  return (
-    <Modal
-      title="Editar Estados"
-      isOpen={node != null}
-      onRequestClose={onRequestClose}
-    >
-      {children}
-    </Modal>
-  );
-};
+  handleInputKeyDown = e => {
+    if (e.key === 'Enter') {
+      this.handleAddClick();
+    }
+  };
+
+  handleAddClick = () => {
+    const newState = this.input.value;
+
+    if (newState === '') {
+      return;
+    }
+
+    if (this.state.states.some(x => x === newState)) {
+      alert('Este estado já foi adicionado');
+      return;
+    }
+
+    this.setState({
+      states: [...this.state.states, newState],
+    });
+
+    this.input.value = '';
+  };
+
+  handleRemoveClick = state => {
+    this.setState({
+      states: this.state.states.filter(x => x !== state),
+    });
+  };
+
+  render() {
+    const { node, onRequestClose } = this.props;
+    let children = null;
+
+    if (node != null) {
+      children = (
+        <div className={styles.container}>
+          <ul className={styles.stateList}>
+            {this.state.states.map(state => (
+              <li key={state}>
+                <span>{state}</span>
+                <Button
+                  onClick={() => this.handleRemoveClick(state)}
+                  title="Remover Estado"
+                >
+                  <i className="fa fa-trash" />
+                </Button>
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles.addState}>
+            <input
+              type="text"
+              onKeyDown={this.handleInputKeyDown}
+              ref={ref => (this.input = ref)}
+            />
+            <Button
+              onClick={this.handleAddClick}
+              title="Adicionar Estado"
+            >
+              <i className="fa fa-plus" />
+            </Button>
+          </div>
+
+          <div className={styles.buttons}>
+            <Button primary onClick={() => alert('salvar')}>Salvar</Button>
+            <Button onClick={onRequestClose}>Cancelar</Button>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <Modal
+        title="Editar Estados"
+        isOpen={node != null}
+        onRequestClose={onRequestClose}
+      >
+        {children}
+      </Modal>
+    );
+  }
+}
 
 EditStates.propTypes = {
   node: PropTypes.object,
