0ba7f49a0e808d26d6bb8b73b3b25b4e66d4445b
fhelwanger <fhelwanger@gmail.com>
2016-09-11 16:47:26 -0300

    Add header menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../Button';
 import styles from './styles.css';
 
-const Header = () => (
-  <div className={styles.header}>
-    <h1 className={styles.title}>Bayes Editor</h1>
-    <Button
-      title="Menu"
-      className={styles.menuButton}
-      onClick={() => console.log('¯\\_(ツ)_/¯')}
-    >
-      <i className="fa fa-bars" />
-    </Button>
-  </div>
-);
+class Header extends Component {
+  state = {
+    menuVisible: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener('click', this.handleWindowClick);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.handleWindowClick);
+  }
+
+  handleWindowClick = () => {
+    this.setState({ menuVisible: false });
+  };
+
+  handleToggleMenu = e => {
+    e.stopPropagation();
+    this.setState({ menuVisible: !this.state.menuVisible });
+  };
+
+  render() {
+    return (
+      <div className={styles.header}>
+        <h1 className={styles.title}>Bayes Editor</h1>
+        <Button
+          title="Menu"
+          className={styles.menuButton}
+          onClick={this.handleToggleMenu}
+        >
+          <i className="fa fa-bars" />
+        </Button>
+        {this.state.menuVisible && (
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>Nova Rede</li>
+            <li className={styles.menuItem}>Abrir Rede</li>
+            <li className={styles.menuItem}>Salvar Rede</li>
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Header;
