e2b9b5a12b57db6935d3c1cb69329f39565ff61b
fhelwanger <fhelwanger@gmail.com>
2016-09-11 17:48:58 -0300

    Remove example network

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { loadState } from './middlewares/persistState';
-import network from './networks/rain-sprinkler-grasswet';
 import App from './components/App';
 
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.css';
 
 const preloadedState = loadState();
-const store = configureStore(preloadedState == null ? network : preloadedState);
+const store = configureStore(preloadedState);
 
 render(
   <Provider store={store}>
