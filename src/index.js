85f6bae77f74c83d31ce70671c55d3aae5488e0b
nolleto <felipenolletonascimento@gmail.com>
2019-03-18 11:06:24 -0300

    Fix lint for utils, store and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import Modal from 'react-modal';
 import configureStore from './store/configureStore';
 import { loadState } from './middlewares/persistState';
 import App from './components/App';
-import Modal from 'react-modal';
 
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.css';
