86b9eca9aa7edb886cef6b1a88948e4ddb675f23
nolleto <felipenolletonascimento@gmail.com>
2019-04-22 16:06:41 -0300

    Add aliases support in cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,8 +10,13 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const webpack = require('@cypress/webpack-preprocessor');
+const resolve = require('../../webpack.config.resolve');
 
-module.exports = (on, config) => { // eslint-disable-line
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+module.exports = (on) => {
+  const options = {
+    webpackOptions: { resolve },
+  };
+
+  on('file:preprocessor', webpack(options));
 };
