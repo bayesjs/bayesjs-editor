2248005f2a3974fc542917f91da7ea73b4673804
nolleto <felipenolletonascimento@gmail.com>
2019-05-03 13:36:51 -0300

    Add jest-css-modules package

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
   snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleNameMapper: {
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
     '^components(.*)$': '<rootDir>/src/components$1',
     '^utils(.*)$': '<rootDir>/src/utils$1',
     '^models(.*)$': '<rootDir>/src/models$1',
