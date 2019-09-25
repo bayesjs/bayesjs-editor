0205b1142a639b735631987531e3f4b8b1983360
nolleto <felipenolletonascimento@gmail.com>
2019-09-25 10:57:00 -0300

    Create `editingNodeCpt` redux state

---
+++ src/actions/editing-node-cpt.js
@@ -0,0 +1,19 @@
+import {
+  EDITING_NODE_CPT,
+  CANCEL_EDITING_NODE_CPT,
+  SAVE_EDITING_NODE_CPT,
+} from 'constants/editing-node-cpt';
+
+export const onEditingNodeCpt = node => ({
+  type: EDITING_NODE_CPT,
+  payload: { node },
+});
+
+export const onCancelEditingNodeCpt = () => ({
+  type: CANCEL_EDITING_NODE_CPT,
+});
+
+export const onSaveEditingNodeCpt = (id, cpt) => ({
+  type: SAVE_EDITING_NODE_CPT,
+  payload: { id, cpt },
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import network from './network';
+import editingNodeCpt from './editing-node-cpt';
 
 const rootReducer = combineReducers({
   network,
+  editingNodeCpt,
   nodes: () => [],
   positions: () => [],
 });
