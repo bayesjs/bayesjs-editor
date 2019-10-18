472fdff1c2375b3f96753fb50d277420d691b521
Sergey Ufocoder <xufocoder@gmail.com>
2019-10-18 12:26:42 +0300

    Extract `colors` util. Use `getRandomColor`

--- src/reducers/subnetwork.js
+++ src/reducers/subnetwork.js
@@ -5,36 +5,7 @@ import {
   REMOVE_SUPER_NODE,
 } from 'actions';
 
-const colorsBuilder = () => {
-  let list = [];
-  const allColors = ['#D84315', '#BF360C', '#FBC02D', '#F9A825', '#F57F17', '#FFA000', '#FF8F00', '#FF6F00', '#F57C00', '#EF6C00', '#E65100', '#388E3C', '#2E7D32', '#689F38', '#558B2F', '#AFB42B', '#0288D1', '#0097A7', '#00838F', '#1976D2', '#1565C0', '#C2185B'];
-  const shuffle = (array) => {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
-
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  };
-
-  return () => {
-    if (list.length === 0) {
-      list = shuffle([...allColors]);
-    }
-
-    return list.shift();
-  };
-};
-
-const colorsNol = colorsBuilder();
+import { getRandomColor } from 'utils/colors';
 
 const formatNetwork = (state) => {
   const { network, positions, nodes } = state;
@@ -44,7 +15,7 @@ const formatNetwork = (state) => {
       ...network,
       nodes,
       positions,
-      color: colorsNol(),
+      color: getRandomColor(),
     };
   }
 
---
+++ src/utils/colors.js
@@ -0,0 +1,36 @@
+export const COLORS = [
+  '#D84315',
+  '#BF360C',
+  '#FBC02D',
+  '#F9A825',
+  '#F57F17',
+  '#FFA000',
+  '#FF8F00',
+  '#FF6F00',
+  '#F57C00',
+  '#EF6C00',
+  '#E65100',
+  '#388E3C',
+  '#2E7D32',
+  '#689F38',
+  '#558B2F',
+  '#AFB42B',
+  '#0288D1',
+  '#0097A7',
+  '#00838F',
+  '#1976D2',
+  '#1565C0',
+  '#C2185B',
+];
+
+const shuffle = array => array.slice().sort(() => Math.random() - 0.5);
+
+export const getRandomColor = (() => {
+  let list = [];
+  return () => {
+    if (!list.length) {
+      list = shuffle(COLORS);
+    }
+    return list.shift();
+  };
+})();
