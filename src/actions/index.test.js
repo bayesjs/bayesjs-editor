075b877c4afa618d66e31c8d1f7ffa65f1a2f4a1
Elias Reis <eliasreis54@gmail.com>
2019-10-18 23:31:03 -0300

    More tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,10 +1,34 @@
-import { NEW_NETWORK, newNetwork } from './index';
+import { NETWORK_KINDS } from '../constants/network';
+import {
+  NEW_NETWORK, LOAD_NETWORK, CHANGE_NETWORK_PROPERTY, ADD_NODE, REMOVE_NODE, ADD_SUPER_NODE } from './index';
+import { newNetwork, loadNetwork, changeNetworkProperty } from './index';
 
-describe('Persisting state actions', () => {
+describe('index file tests', () => {
   it('newNetwork ', () => {
     expect(newNetwork).toBeInstanceOf(Function);
     newNetwork((ret) => {
-      expect(ret.type).toHaveBeenCalledTimes(NEW_NETWORK);
+      expect(ret.type).toEqual(NEW_NETWORK);
+      expect(ret.kind).toEqual(NETWORK_KINDS.BN);
+    });
+  });
+
+  it('loadNetwork', () => {
+    const state = { value: '1' };
+    expect(loadNetwork).toBeInstanceOf(Function);
+    loadNetwork(state, (ret) => {
+      expect(ret.type).toEqual(LOAD_NETWORK);
+      expect(ret.payload).toEqual({
+        value: '1',
+      });
+    });
+  });
+
+  it('changeNetworkProperty', () => {
+    const params = { name: 'test', value: 'test' };
+    expect(changeNetworkProperty).toBeInstanceOf(Function);
+    changeNetworkProperty(params, (ret) => {
+      expect(ret.type).toEqual(CHANGE_NETWORK_PROPERTY);
+      expect(ret.payload).toEqual(params);
     });
   });
 });
