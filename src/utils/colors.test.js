f92f1d3e303248594b7f9709b6cd88aa8feeb32a
Sergey Ufocoder <xufocoder@gmail.com>
2019-10-20 17:01:35 +0300

    Improve test for `src/utils/colors` lib

--- src/utils/colors.test.js
+++ src/utils/colors.test.js
@@ -1,16 +1,27 @@
+import { times } from 'ramda';
 import { COLORS, getRandomColor } from './colors';
 
 describe('Colors Utils', () => {
   describe('getRandomColor', () => {
-    it('returns truthy (keyCode)', () => {
-      const firstRandomColor = getRandomColor();
-      const secondRandomColor = getRandomColor();
+    beforeAll(() => {
+      jest.spyOn(Math, 'random').mockReturnValue(0.5);
+    });
 
-      if (COLORS.length === 1) {
-        expect(firstRandomColor).toBe(secondRandomColor);
-      } else {
-        expect(firstRandomColor).not.toBe(secondRandomColor);
-      }
+    afterAll(() => {
+      Math.random.mockRestore();
+    });
+
+    it('returns "#388E3C" on the first time', () => {
+      expect(getRandomColor()).toBe('#388E3C');
+    });
+
+    it('returns "#D84315" on the second time', () => {
+      expect(getRandomColor()).toBe('#D84315');
+    });
+
+    it('returns "#388E3C" on the twenty first time', () => {
+      times(getRandomColor, COLORS.length - 2);
+      expect(getRandomColor()).toBe('#388E3C');
     });
   });
 });
