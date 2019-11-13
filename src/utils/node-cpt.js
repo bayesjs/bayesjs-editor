26680938ac0d88e2839a56a7d95b95f791028b0f
nolleto <felipenolletonascimento@gmail.com>
2019-11-13 14:27:13 -0300

    Add new methods in node-cpt utils

--- src/utils/node-cpt.js
+++ src/utils/node-cpt.js
@@ -1,11 +1,117 @@
 import {
-  is,
+  T,
+  add,
+  all,
+  always,
+  applySpec,
   assoc,
-  set,
+  complement,
+  concat,
+  cond,
+  converge,
+  curry,
+  difference,
+  dissoc,
+  divide,
+  equals,
+  evolve,
+  flatten,
+  flip,
+  identity,
+  ifElse,
+  includes,
+  invertObj,
+  is,
+  head,
+  keys,
+  length,
   lensPath,
+  lensProp,
+  map,
+  mergeRight,
+  nthArg,
+  objOf,
+  omit,
+  over,
+  path,
+  pick,
+  pipe,
+  prop,
+  reject,
+  set,
+  subtract,
+  sum,
+  uniqBy,
+  useWith,
+  values,
 } from 'ramda';
+import { containsParentInNode, isNodeWithoutParents } from 'validations/node';
 
+import { createNodeCombinations } from './combinations';
+import { filterNodeParents } from './node-parents';
+import { findNodeById } from './node';
+import { roundValue } from './math';
+
+const propId = prop('id');
+const propCpt = prop('cpt');
+const propStates = prop('states');
+const propParents = prop('parents');
 const isArray = is(Array);
+const nthArg0 = nthArg(0);
+const nthArg1 = nthArg(1);
+const includesFlipped = flip(includes);
+const getCptValue = converge(divide, [always(1), length]);
+const getCptValueRounded = pipe(getCptValue, roundValue);
+const mapAndFlatten = pipe(map, flatten);
+const uniqByWhen = uniqBy(prop('when'));
+const objectLenght = pipe(keys, length);
+const sumValues = pipe(values, sum);
+const getFirstKey = pipe(keys, head);
+const getMissingValueFromCpt = pipe(sumValues, subtract(1));
+const filterCptWhenByKeys = useWith(evolve, [pipe(pick, objOf('when'))]);
+const mapWhen = useWith(map, [filterCptWhenByKeys, identity]);
+const hasOnlyThisParent = useWith(all, [pipe(propId, equals), propParents]);
+const getValueForEachCpt = useWith(pipe(divide, roundValue), [identity, length]);
+const hasNoParent = useWith(complement(containsParentInNode), [
+  propId,
+  identity,
+]);
+
+const getMissingValueForEachCpt = converge(divide, [
+  pipe(nthArg0, getMissingValueFromCpt),
+  pipe(nthArg0, objectLenght),
+]);
+
+const fixCptFloatingPoint = converge(
+  over,
+  [
+    pipe(nthArg0, getFirstKey, lensProp),
+    pipe(nthArg0, getMissingValueFromCpt, roundValue, add),
+    nthArg0,
+  ],
+);
+
+const balanceCptValues = converge(pipe(map, fixCptFloatingPoint), [
+  pipe(nthArg0, getMissingValueForEachCpt, roundValue, add),
+  nthArg0,
+]);
+
+const addKeyAndValueInWhen = (key, value) =>
+  set(lensPath(['when', key]), value);
+
+const addNewStatesInCptWithValue = (value, states, cpt) =>
+  mergeRight(
+    cpt,
+    map(always(getValueForEachCpt(value, states)), invertObj(states)),
+  );
+
+const getDiffStates = applySpec({
+  deletedStates: flip(difference),
+  newStates: difference,
+});
+
+const renamePropKey = curry((oldKey, newKey, obj) =>
+  assoc(newKey, prop(oldKey, obj), dissoc(oldKey, obj)));
 
 export const updateCptValue = (cpt, value, state, index) => {
   if (isArray(cpt)) {
@@ -13,3 +119,121 @@ export const updateCptValue = (cpt, value, state, index) => {
   }
   return assoc(state, value, cpt);
 };
+
+export const createCpt = converge(pipe(map, fixCptFloatingPoint), [
+  pipe(nthArg0, getCptValueRounded, always),
+  pipe(nthArg0, invertObj),
+]);
+
+const cptObjectToArray = (nodeParent, node) =>
+  map(
+    state => ({
+      when: objOf(propId(nodeParent), state),
+      then: propCpt(node),
+    }),
+    propStates(nodeParent),
+  );
+
+const addCptArrayItem = (nodeParent, node) => {
+  const id = propId(nodeParent);
+
+  return mapAndFlatten(
+    newState => map(addKeyAndValueInWhen(id, newState), propCpt(node)),
+    propStates(nodeParent),
+  );
+};
+
+export const addNodeParentInCpt = ifElse(
+  pipe(nthArg1, isNodeWithoutParents),
+  cptObjectToArray,
+  addCptArrayItem,
+);
+
+const removeParentInCptWhenNodeHasOneParent = path(['cpt', 0, 'then']);
+
+const removeParentInCptWhenNodeHasMoreThanOneParent = (nodeParent, node) =>
+  uniqByWhen(
+    mapWhen(filterNodeParents(propId(nodeParent), node), propCpt(node)),
+  );
+
+export const removeNodeParentInCpt = cond([
+  [hasNoParent, pipe(nthArg1, propCpt)],
+  [hasOnlyThisParent, pipe(nthArg1, removeParentInCptWhenNodeHasOneParent)],
+  [T, removeParentInCptWhenNodeHasMoreThanOneParent],
+]);
+
+const changeCptStates = curry((nextStates, cpt) => {
+  const { deletedStates, newStates } = getDiffStates(nextStates, keys(cpt));
+  const valueFromDeletedStates = roundValue(sumValues(pick(deletedStates, cpt)));
+
+  return balanceCptValues(
+    omit(
+      deletedStates,
+      addNewStatesInCptWithValue(valueFromDeletedStates, newStates, cpt),
+    ),
+  );
+});
+
+const updateStatesInCptObject = useWith(changeCptStates, [identity, propCpt]);
+
+const updateStatesInCptArray = (nextStates, node) =>
+  map(
+    evolve({
+      then: changeCptStates(nextStates),
+    }),
+    propCpt(node),
+  );
+
+export const updateStatesInCpt = ifElse(
+  pipe(nthArg1, isNodeWithoutParents),
+  updateStatesInCptObject,
+  updateStatesInCptArray,
+);
+
+export const updateNodeParentIdInCpt = (nodeId, nextId, node) =>
+  map(
+    evolve({
+      when: renamePropKey(nodeId, nextId),
+    }),
+    propCpt(node),
+  );
+
+const createWhensForNodeWithStates = (parentId, newStates, nodes, node) => {
+  const nodeParents = map(id => findNodeById(id, nodes), node.parents);
+  const parents = map(
+    ({ id, states }) => ({
+      id,
+      states: equals(id, parentId) ? newStates : states,
+    }),
+    nodeParents,
+  );
+
+  return createNodeCombinations(parents);
+};
+
+const removeCptsForNodeWithStates = (parentId, deletedStates, node) =>
+  reject(
+    pipe(path(['when', parentId]), includesFlipped(deletedStates)),
+    propCpt(node),
+  );
+
+const createCptsForNodeWithStates = (parentId, newStates, nodes, node) =>
+  map(
+    pipe(objOf('when'), assoc('then', createCpt(propStates(node)))),
+    createWhensForNodeWithStates(parentId, newStates, nodes, node),
+  );
+
+export const updateNodeParentStatesInCpt = curry(
+  (parentId, nextStates, nodes, node) => {
+    const nodeParent = findNodeById(parentId, nodes);
+    const { deletedStates, newStates } = getDiffStates(
+      nextStates,
+      propStates(nodeParent),
+    );
+
+    return concat(
+      removeCptsForNodeWithStates(parentId, deletedStates, node),
+      createCptsForNodeWithStates(parentId, newStates, nodes, node),
+    );
+  },
+);
--- src/utils/node-cpt.test.js
+++ src/utils/node-cpt.test.js
@@ -1,8 +1,15 @@
+import SimpleNetwork from 'json-templates/networks/simple.json';
 import {
+  addNodeParentInCpt,
+  createCpt,
+  removeNodeParentInCpt,
   updateCptValue,
+  updateNodeParentIdInCpt,
+  updateNodeParentStatesInCpt,
+  updateStatesInCpt,
 } from './node-cpt';
 
-describe('Node Validations', () => {
+describe('Node Cpt Utils', () => {
   describe('updateCptValue', () => {
     describe('When cpt is a object', () => {
       const cpt = {
@@ -59,6 +66,545 @@ describe('Node Validations', () => {
               T: 0.5,
               F: 0.3,
             },
+          },
+        ]);
+      });
+    });
+  });
+
+  describe('createCpt', () => {
+    describe('When node has two states', () => {
+      const states = ['True', 'False'];
+
+      it('returns a object with all keys with the same value', () => {
+        expect(createCpt(states)).toEqual({
+          True: 0.5,
+          False: 0.5,
+        });
+      });
+    });
+
+    describe('When node has three states (floating point)', () => {
+      const states = ['State_1', 'State_2', 'State_3'];
+
+      it('returns a object with all keys value truncated and the first one with the missing value', () => {
+        expect(createCpt(states)).toEqual({
+          State_1: 0.33333334,
+          State_2: 0.33333333,
+          State_3: 0.33333333,
+        });
+      });
+    });
+  });
+
+  describe('addNodeParentInCpt', () => {
+    describe('When node has no parents', () => {
+      const node = {
+        id: 'Node 1',
+        parents: [],
+        cpt: {
+          True: 0.5,
+          False: 0.5,
+        },
+      };
+      const nodeParent = {
+        id: 'Node 2',
+        states: ['True', 'False'],
+      };
+
+      it('returns a cpt array', () => {
+        expect(addNodeParentInCpt(nodeParent, node)).toEqual([
+          {
+            then: { False: 0.5, True: 0.5 },
+            when: { 'Node 2': 'True' },
+          }, {
+            then: { False: 0.5, True: 0.5 },
+            when: { 'Node 2': 'False' },
+          },
+        ]);
+      });
+    });
+
+    describe('When node has parents', () => {
+      const node = {
+        id: 'Node 3',
+        cpt: [
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ],
+      };
+      const nodeParent = {
+        id: 'Node 4',
+        states: ['True', 'False'],
+
+      };
+
+      it('returns cpt array with news items', () => {
+        expect(addNodeParentInCpt(nodeParent, node)).toEqual([
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ]);
+      });
+    });
+  });
+
+  describe('removeNodeParentInCpt', () => {
+    describe('When node doens not have the parent', () => {
+      const node = {
+        id: 'Node 3',
+        parents: [],
+        states: ['True', 'False'],
+        cpt: { True: 0.5, False: 0.5 },
+      };
+      const nodeParent = {
+        id: 'Node 1',
+        parents: [],
+        states: ['True', 'False'],
+        cpt: { 'State 1': 0.5, 'State 2': 0.5 },
+      };
+
+      it('returns cpt', () => {
+        expect(removeNodeParentInCpt(nodeParent, node)).toEqual({ True: 0.5, False: 0.5 });
+      });
+    });
+
+    describe('When node has only this parent', () => {
+      const nodeParent = {
+        id: 'Node 4',
+      };
+      const node = {
+        parents: ['Node 4'],
+        cpt: [
+          {
+            when: { 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ],
+      };
+
+      it('returns the first "then" as cpt', () => {
+        expect(removeNodeParentInCpt(nodeParent, node)).toEqual({
+          True: 0.5,
+          False: 0.5,
+        });
+      });
+    });
+
+    describe('When node has not only this parent', () => {
+      const nodeParent = {
+        id: 'Node 4',
+        states: ['True', 'False'],
+      };
+      const node = {
+        parents: ['Node 1', 'Node 2', 'Node 4'],
+        cpt: [
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False', 'Node 4': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False', 'Node 4': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ],
+      };
+
+      it('removes parent node from "when"', () => {
+        expect(removeNodeParentInCpt(nodeParent, node)).toEqual([
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ]);
+      });
+    });
+  });
+
+  describe('updateStatesInCpt', () => {
+    describe('When node has no parents', () => {
+      const node = {
+        id: 'Node 1',
+        states: [
+          'True',
+          'False',
+        ],
+        parents: [],
+        cpt: {
+          State_1: 0.25,
+          State_2: 0.25,
+          State_3: 0.25,
+          State_4: 0.25,
+        },
+      };
+
+      describe('and is adding a new state', () => {
+        const states = ['State_1', 'State_2', 'State_3', 'State_4', 'State_5'];
+
+        it('adds new state with zero as value', () => {
+          expect(updateStatesInCpt(states, node)).toEqual({
+            State_1: 0.25,
+            State_2: 0.25,
+            State_3: 0.25,
+            State_4: 0.25,
+            State_5: 0,
+          });
+        });
+      });
+
+      describe('and is removing a state', () => {
+        const states = ['State_1', 'State_2'];
+
+        it('removes the states and balance the cpt values', () => {
+          expect(updateStatesInCpt(states, node)).toEqual({
+            State_1: 0.5,
+            State_2: 0.5,
+          });
+        });
+      });
+
+      describe('and is adding and removing states', () => {
+        const states = ['State_1', 'State_2', 'State_3', 'State_New'];
+
+        it('removes/adds the states and adds the removed value in new states', () => {
+          expect(updateStatesInCpt(states, node)).toEqual({
+            State_1: 0.25,
+            State_2: 0.25,
+            State_3: 0.25,
+            State_New: 0.25,
+          });
+        });
+      });
+    });
+
+    describe('When node has parents', () => {
+      const node = {
+        id: 'Node 3',
+        states: ['True', 'False'],
+        parents: ['Node 2', 'Node 1'],
+        cpt: [
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'True', 'Node 1': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 2': 'False', 'Node 1': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ],
+      };
+
+      describe('and is adding a new state', () => {
+        const states = ['True', 'False', 'New State'];
+
+        it('adds new state with zero as value', () => {
+          expect(updateStatesInCpt(states, node)).toEqual([
+            {
+              when: { 'Node 1': 'True', 'Node 2': 'True' },
+              then: { 'New State': 0, True: 0.5, False: 0.5 },
+            },
+            {
+              when: { 'Node 1': 'True', 'Node 2': 'False' },
+              then: { 'New State': 0, True: 0.5, False: 0.5 },
+            },
+            {
+              when: { 'Node 1': 'False', 'Node 2': 'True' },
+              then: { 'New State': 0, True: 0.5, False: 0.5 },
+            },
+            {
+              when: { 'Node 1': 'False', 'Node 2': 'False' },
+              then: { 'New State': 0, True: 0.5, False: 0.5 },
+            },
+          ]);
+        });
+      });
+
+      describe('and is removing a state', () => {
+        const states = ['True'];
+
+        it('removes the state and balance the cpt values', () => {
+          expect(updateStatesInCpt(states, node)).toEqual([
+            {
+              when: { 'Node 1': 'True', 'Node 2': 'True' },
+              then: { True: 1 },
+            },
+            {
+              when: { 'Node 1': 'True', 'Node 2': 'False' },
+              then: { True: 1 },
+            },
+            {
+              when: { 'Node 1': 'False', 'Node 2': 'True' },
+              then: { True: 1 },
+            },
+            {
+              when: { 'Node 1': 'False', 'Node 2': 'False' },
+              then: { True: 1 },
+            },
+          ]);
+        });
+      });
+
+      describe('and is adding and removing states', () => {
+        const states = ['True', 'New State'];
+
+        it('removes/adds the states and adds the removed value in new states', () => {
+          expect(updateStatesInCpt(states, node)).toEqual([
+            {
+              when: { 'Node 1': 'True', 'Node 2': 'True' },
+              then: { 'New State': 0.5, True: 0.5 },
+            },
+            {
+              when: { 'Node 1': 'True', 'Node 2': 'False' },
+              then: { 'New State': 0.5, True: 0.5 },
+            },
+            {
+              when: { 'Node 1': 'False', 'Node 2': 'True' },
+              then: { 'New State': 0.5, True: 0.5 },
+            },
+            {
+              when: { 'Node 1': 'False', 'Node 2': 'False' },
+              then: { 'New State': 0.5, True: 0.5 },
+            },
+          ]);
+        });
+      });
+    });
+  });
+
+  describe('updateNodeParentIdInCpt', () => {
+    const id = 'Node 2';
+    const nextId = 'New ID';
+    const node = {
+      id: 'Node 3',
+      cpt: [
+        {
+          when: { 'Node 2': 'True', 'Node 1': 'True' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'Node 2': 'False', 'Node 1': 'True' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'Node 2': 'True', 'Node 1': 'False' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'Node 2': 'False', 'Node 1': 'False' },
+          then: { True: 0.5, False: 0.5 },
+        },
+      ],
+    };
+
+    it('updates "when" with new id', () => {
+      expect(updateNodeParentIdInCpt(id, nextId, node)).toEqual([
+        {
+          when: { 'New ID': 'True', 'Node 1': 'True' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'New ID': 'False', 'Node 1': 'True' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'New ID': 'True', 'Node 1': 'False' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'New ID': 'False', 'Node 1': 'False' },
+          then: { True: 0.5, False: 0.5 },
+        },
+      ]);
+    });
+  });
+
+  describe('updateNodeParentStatesInCpt', () => {
+    const parentId = 'Node 1';
+    const node = {
+      id: 'Node 3',
+      states: ['True', 'False'],
+      parents: ['Node 2', 'Node 1'],
+      cpt: [
+        {
+          when: { 'Node 1': 'True', 'Node 2': 'True' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'Node 1': 'True', 'Node 2': 'False' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'Node 1': 'False', 'Node 2': 'True' },
+          then: { True: 0.5, False: 0.5 },
+        },
+        {
+          when: { 'Node 1': 'False', 'Node 2': 'False' },
+          then: { True: 0.5, False: 0.5 },
+        },
+      ],
+    };
+
+    describe('When is adding a new state', () => {
+      const nextStates = ['True', 'False', 'New State'];
+
+      it('adds new cpt item with new states', () => {
+        expect(updateNodeParentStatesInCpt(parentId, nextStates, SimpleNetwork, node)).toEqual([
+          {
+            when: { 'Node 1': 'True', 'Node 2': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'True', 'Node 2': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'False', 'Node 2': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'False', 'Node 2': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'New State', 'Node 2': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'New State', 'Node 2': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ]);
+      });
+    });
+
+    describe('When is removing a state', () => {
+      const nextStates = ['True'];
+
+      it('removes cpt items with removed states', () => {
+        expect(updateNodeParentStatesInCpt(parentId, nextStates, SimpleNetwork, node)).toEqual([
+          {
+            when: { 'Node 1': 'True', 'Node 2': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'True', 'Node 2': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+        ]);
+      });
+    });
+
+    describe('When is adding and removing states', () => {
+      const nextStates = ['True', 'New State'];
+
+      it('removes/adds cpt items', () => {
+        expect(updateNodeParentStatesInCpt(parentId, nextStates, SimpleNetwork, node)).toEqual([
+          {
+            when: { 'Node 1': 'True', 'Node 2': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'True', 'Node 2': 'False' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'New State', 'Node 2': 'True' },
+            then: { True: 0.5, False: 0.5 },
+          },
+          {
+            when: { 'Node 1': 'New State', 'Node 2': 'False' },
+            then: { True: 0.5, False: 0.5 },
           },
         ]);
       });
