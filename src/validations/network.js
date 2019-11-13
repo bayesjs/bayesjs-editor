d79dfade1f1faf50a846ef55f8bec26bbfcd3f45
nolleto <felipenolletonascimento@gmail.com>
2019-11-13 14:26:33 -0300

    Add new cycle validation method

--- src/validations/network.js
+++ src/validations/network.js
@@ -1,30 +1,35 @@
 import {
-  map,
-  isEmpty,
-  reject,
+  any,
+  append,
+  complement,
   equals,
   filter,
+  identity,
+  isEmpty,
+  lensPath,
+  map,
+  over,
+  pick,
   pipe,
   prop,
-  pick,
-  includes,
-  any,
-  complement,
+  reject,
+  useWith,
 } from 'ramda';
+import { findIndexNodeById } from 'utils/node';
+import { containsParentInNode } from './node';
 
 const isNotEmpty = complement(isEmpty);
 const propId = prop('id');
 const propParents = prop('parents');
 const mapIdAndParents = map(pick(['id', 'parents']));
-
 const hasEmptyParents = pipe(propParents, isEmpty);
 const filterEmptyParentNodes = filter(hasEmptyParents);
 const anyNodesHasNoEmptyParents = any(complement(hasEmptyParents));
 
-const hasParentsWithId = id => pipe(propParents, includes(id));
-
-const getNodesWithParents = (parentId, nodes) =>
-  filter(hasParentsWithId(parentId), nodes);
+const getNodesWithParents = useWith(
+  filter,
+  [containsParentInNode, identity],
+);
 
 const removeParentFromNode = (node, parentId) => {
   node.parents = reject(equals(parentId), propParents(node));
@@ -52,3 +57,12 @@ export const hasCycles = (nodes) => {
 
   return anyNodesHasNoEmptyParents(clonedNodes);
 };
+
+const addNodeParentInNodes = (id, parentId, nodes) => over(
+  lensPath([findIndexNodeById(id, nodes), 'parents']),
+  append(parentId),
+  nodes,
+);
+
+export const hasCycleAddingNodeParent = (parentId, nodeId, nodes) =>
+  hasCycles(addNodeParentInNodes(nodeId, parentId, nodes));
